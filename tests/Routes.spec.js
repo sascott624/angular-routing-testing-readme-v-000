7f79017adf022155ce26a0acb61ce24e16d9bae5
Savannah Scott <sascott624@gmail.com>
2016-08-29 20:45:49 -0400

    Done.

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -14,6 +14,30 @@ describe('Routes', function () {
 			state = $state.get('settings');
 
 			expect(state.url).toEqual('/settings');
+
+			expect(state.templateUrl).toEqual('views/settings.html')
 		});
 	});
+
+	describe('User', function(){
+		var state;
+
+		it('should have the correct URL', function(){
+			state = $state.get('settings.user');
+			expect(state.url).toEqual('/user');
+			expect(state.templateUrl).toEqual('views/settings/user.html');
+		})
+	})
+
+	describe('Notifications', function(){
+		var state;
+
+		it('should have the correct URL', function(){
+			state = $state.get('settings.notifications');
+			expect(state.url).toEqual('/notifications');
+			expect(state.templateUrl).toEqual('views/settings/notifications.html');
+		})
+	})
+
+
 });
